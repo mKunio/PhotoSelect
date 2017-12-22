e2120d146b20ec16aace6b15e6b47d4404fd909f
zhouhancheng <1808177097@qq.com>
2017-12-22 17:09:35 +0800

    创建项目

---
+++ app/src/main/java/mlearn/sabachina/com/cn/mygithub/MainActivity.java
@@ -0,0 +1,13 @@
+package mlearn.sabachina.com.cn.mygithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
